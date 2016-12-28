baa4d431fa8c9836fc23054d5c80aae1d883be9d
Bogna Konstanty <rosiakb@gmail.com>
2016-12-28 17:53:15 +0100

    add adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,19 @@
-/// plik scripts.js
-
-
-  function getTriangleArea(a, h) {
+function getTriangleArea(a, h) {
   
 	if ((a > 0) && (h > 0)) { 
-		console.log(' a wynosi: ' + a +' h wynosi: ' + h +' a i h mają wartość większą od zera');
+		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają wartość większą od zera');
     var result = a*h/2;
     return result;
 	}else if ((a < 0) && (h < 0))  {
-		console.log(' a wynosi: ' + a +' h wynosi: ' + h +' a i h mają wartość mniejszą od zera');
+		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają wartość mniejszą od zera');
 	}else  {
 		console.log(' Nieprawidłowe dane');
 	}
 
 }
 var triangle1Area = getTriangleArea(10,15);
-console.log( getTriangleArea(10,15) )
-var triangle2Area = getTriangleArea(10,15);
-console.log( getTriangleArea(5,2) )
+console.log('Wynik działania: '+ triangle1Area);
+var triangle2Area = getTriangleArea(5,2);
+console.log('Wynik działania: '+ triangle2Area);
 var triangle3Area = getTriangleArea(7, 4);
-console.log( getTriangleArea(7,4) )
-
-
-
-
-
+console.log('Wynik działania: '+ triangle3Area);
