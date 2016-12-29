3a78bf8aff23914178a653d77826d475f4c67aaa
Bogna Konstanty <rosiakb@gmail.com>
2016-12-29 09:58:19 +0100

    work code up

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-function getTriangleArea(a, h) {
-  
-	if ((a > 0) && (h > 0)) { 
-		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają wartość większą od zera');
-    var result = a*h/2;
-    return result;
-	}else if ((a < 0) && (h < 0))  {
-		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają wartość mniejszą od zera');
-	}else  {
-		console.log(' Nieprawidłowe dane');
-	}
-
-}
 var triangle1Area = getTriangleArea(10,15);
-console.log('Wynik działania: '+ triangle1Area);
 var triangle2Area = getTriangleArea(5,2);
-console.log('Wynik działania: '+ triangle2Area);
 var triangle3Area = getTriangleArea(7, 4);
-console.log('Wynik działania: '+ triangle3Area);
+
+function getTriangleArea(a, h) {
+	if ((a > 0) && (h > 0)) { 
+    var result = a*h/2;
+		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają     wartość większą od zera'+' Wynik działania: '+result);
+    return result;
+	  }else  {
+		  console.log(' Nieprawidłowe dane');
+	  }
+  }
+
+
+
+
