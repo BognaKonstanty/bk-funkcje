2a325499d9b2e79a2f6c7ecf7a5d4c6459a90da7
Bogna Konstanty <rosiakb@gmail.com>
2016-12-29 10:17:00 +0100

    work code up

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var triangle2Area = getTriangleArea(5,2);
 var triangle3Area = getTriangleArea(7, 4);
 
 function getTriangleArea(a, h) {
-	if ((a > 0) && (h > 0)) { 
+	if (a > 0) && (h > 0) { 
     var result = a*h/2;
 		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają     wartość większą od zera'+' Wynik działania: '+result);
     return result;
