7dbf3f0b91faba091ac0f4c128aebb4be7fdf97d
Bogna Konstanty <rosiakb@gmail.com>
2016-12-29 10:36:37 +0100

    work code up

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-var triangle1Area = getTriangleArea(10,15);
-var triangle2Area = getTriangleArea(5,2);
-var triangle3Area = getTriangleArea(7, 4);
 
 function getTriangleArea(a, h) {
 	if (a > 0) && (h > 0) { 
     	var result = a*h/2;
 		console.log(' a wynosi: ' + a +', h wynosi: ' + h +', a i h mają     wartość większą od zera'+' Wynik działania: '+result);
     	return result;
-	}else  {
+	} 	else  {
 		console.log(' Nieprawidłowe dane');
-	 }
+	}
 }
 
 
-
+var triangle1Area = getTriangleArea(10,15);
+var triangle2Area = getTriangleArea(5,2);
+var triangle3Area = getTriangleArea(7, 4);
 
